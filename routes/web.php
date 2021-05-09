7f3441cb689b878ebfc80a169aaa38fceefcb6e1
lena-sever <remont001@list.ru>
2021-05-09 18:04:21 +0300

    try create breeze num 2

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Http\Controllers\IndexController;
-use App\Http\Controllers\NewsController;
-use App\Http\Controllers\CategoryController;
-use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [IndexController::class, 'index'])->name('index');
-Route::get('/project', [ProjectController::class, 'index'])->name('project');
-Route::get('/news', [NewsController::class, 'index'])->name('news.index');
-Route::get('/news/{news}', [NewsController::class, 'show'])->name('news.show');
-Route::get('/categories', [CategoryController::class, 'index'])->name('cat.index');
-Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('cat.show');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
+require __DIR__.'/auth.php';
